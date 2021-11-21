93164f2f8a965c536b963e38f2c8c4948cda69c4
Kurt Kremitzki <kurt@kwk.systems>
2021-11-21 01:32:23 +0000

    Add basic init.lua & packer.nvim config

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,17 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local packer_url = 'https://github.com/wbthomason/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', packer_url, install_path})
+end
+
+return require('packer').startup(function(use)
+  -- My plugins here
+  use 'wbthomason/packer.nvim'
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
