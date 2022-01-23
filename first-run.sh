8d7353a0d3e18e2fcddbc9a7304997d59054ebb1
Kurt Kremitzki <kurt@kwk.systems>
2022-01-23 18:56:40 +0000

    Check if packer repo already exists

--- first-run.sh
+++ first-run.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
-sudo apt-get install neovim python3-venv direnv fzf ripgrep stow tmux cmake golang npm fd-find bat wget bash-completion
-git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+DEV_PKGS="neovim python3-venv direnv fzf ripgrep stow tmux cmake golang npm \
+          fd-find bat wget bash-completion"
+PACKER_REPO="https://github.com/wbthomason/packer.nvim"
+PACKER_TARGET="$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim"
+
+# shellcheck disable=SC2046 # Intended splitting of DEV_PKGS
+sudo apt-get install "$DEV_PKGS"
+[ ! -d "$PACKER_TARGET" ] && \
+git clone --depth 1 "$PACKER_REPO" "$PACKER_TARGET"
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
