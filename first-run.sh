9802cdab8492c023d997970c61f717313f80cc60
Kurt Kremitzki <kurt@kwk.systems>
2022-01-23 22:53:05 +0000

    Append to nvim install logs

--- first-run.sh
+++ first-run.sh
@@ -16,5 +16,5 @@ stow -R .
 sudo update-alternatives --set editor /usr/bin/nvim
 sudo update-alternatives --set vim /usr/bin/nvim
 sudo update-alternatives --set vi /usr/bin/nvim
-nvim --headless -c "autocmd User PackerComplete quitall" -c "PackerSync" > nvim-packer.log 2>&1
-nvim --headless -c "LspInstall --sync ${langservers}" -c q > nvim-lsp.log 2>&1
+nvim --headless -c "autocmd User PackerComplete quitall" -c "PackerSync" >> nvim-packer.log 2>&1
+nvim --headless -c "LspInstall --sync ${langservers}" -c q >> nvim-lsp.log 2>&1
