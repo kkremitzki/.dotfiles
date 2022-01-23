b4e941b6056f73d02811b969b53143a1ec0feee5
Kurt Kremitzki <kurt@kwk.systems>
2022-01-23 18:39:30 +0000

    Add apt install and PackerSync steps for startup script

--- first-run.sh
+++ first-run.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+sudo apt-get install neovim python3-venv direnv fzf ripgrep stow tmux cmake golang npm fd-find bat wget bash-completion
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
