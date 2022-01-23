533490b830886024b01f6d8ff489fd046c13f298
Kurt Kremitzki <kurt@kwk.systems>
2022-01-23 19:42:36 +0000

    Unquote apt install and include stow step

--- first-run.sh
+++ first-run.sh
@@ -5,7 +5,8 @@ PACKER_REPO="https://github.com/wbthomason/packer.nvim"
 PACKER_TARGET="$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim"
 
 # shellcheck disable=SC2046 # Intended splitting of DEV_PKGS
-sudo apt-get install "$DEV_PKGS"
+sudo apt-get install $DEV_PKGS
 [ ! -d "$PACKER_TARGET" ] && \
 git clone --depth 1 "$PACKER_REPO" "$PACKER_TARGET"
+stow -R .
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
