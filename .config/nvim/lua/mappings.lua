bb29c48358a9af0f01b0d787ce472842b5ebcfa0
Kurt Kremitzki <kurt@kwk.systems>
2022-02-01 03:14:14 +0000

    Improve edit-vimrc mapping with new tab and change-dir effect

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,2 +1,2 @@
-vim.api.nvim_set_keymap('n', '<Leader>ev', ':edit ~/.config/nvim<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>ev', ':tabnew ~/.config/nvim<CR>:tcd ~/.config/nvim<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<Leader>sv', ':source $MYVIMRC<CR>', { noremap = true })
