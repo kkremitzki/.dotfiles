56d8037c64c7a048e52365ab4e7cff131d4f9d95
Kurt Kremitzki <kurt@kwk.systems>
2022-02-01 04:20:20 +0000

    Move more editor config to lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,6 +14,18 @@ vim.cmd([[
 
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
+-- Backspace spaces as if they were a 4-length tabstop
+vim.opt.softtabstop = 4
 vim.opt.completeopt = 'menu,menuone,noselect'
 
 vim.opt.ttimeoutlen = 20
+vim.opt.updatetime = 250
+
+-- Ignore case only when all-lowercase is used
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+-- Allow mouse use in everything but `hit-enter` and `more-prompt`
+vim.opt.mouse = 'a'
+-- Default split behavior seems a bit disorienting with current placement of
+-- text replaced with new split, so split below instead
+vim.opt.splitbelow = true
