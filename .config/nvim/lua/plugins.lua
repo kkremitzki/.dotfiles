6944ef237bf85bb3d77360a68e854a237c808c09
Kurt Kremitzki <kurt@kwk.systems>
2023-03-07 22:36:42 -0600

    Re-enable nvim-cmp handling of nvim cmdline

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -224,14 +224,14 @@ return require('packer').startup(function(use)
       })
 
       -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-      -- cmp.setup.cmdline(':', {
-      --   mapping = cmp.mapping.preset.cmdline(),
-      --   sources = cmp.config.sources({
-      --     { name = 'path' }
-      --   }, {
-      --     { name = 'cmdline' }
-      --   })
-      -- })
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' }
+        }, {
+          { name = 'cmdline' }
+        })
+      })
 
     end
   }
