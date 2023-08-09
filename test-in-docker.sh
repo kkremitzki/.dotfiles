b9c3fe5dccacd4107364a74d8d12c8746886cb14
Kurt Kremitzki <kurt@kwk.systems>
2023-08-08 20:48:43 -0500

    Fix shell cut quoting

--- test-in-docker.sh
+++ test-in-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-DOCKER_IP="$(ip a s docker0 | grep 'inet ' | cut -'d ' -f6 | cut -d'/' -f1)"
+DOCKER_IP="$(ip a s docker0 | grep 'inet ' | cut -d' ' -f6 | cut -d'/' -f1)"
 export http_proxy="http://${DOCKER_IP}:3142"
 export DOCKER_BUILDKIT=1
 docker build --build-arg http_proxy -f Dockerfile -t dotfiles .
