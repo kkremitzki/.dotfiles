911ff42e76ed3b0031573970a41b1657a7bee087
Kurt Kremitzki <kurt@kwk.systems>
2023-08-09 22:50:28 -0500

    Use volume for dotfiles in Docker

--- test-in-docker.sh
+++ test-in-docker.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+SOURCE_DIR=$(dirname "$0")
+# Resolve e.g. `.` to an absolute path
+SOURCE_DIR=$(cd "${SOURCE_DIR}" || exit; pwd)
+TARGET_DIR=/home/debian/.dotfiles
 DOCKER_IP="$(ip a s docker0 | grep 'inet ' | cut -d' ' -f6 | cut -d'/' -f1)"
 export http_proxy="http://${DOCKER_IP}:3142"
 export DOCKER_BUILDKIT=1
 docker build --build-arg http_proxy -f Dockerfile -t dotfiles .
-docker run -e http_proxy --rm -it dotfiles
+docker run -v "${SOURCE_DIR}:${TARGET_DIR}" -e http_proxy --rm -it dotfiles
