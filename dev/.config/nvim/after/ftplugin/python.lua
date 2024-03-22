6c2b74c6a55cd586850b9104da303a7c38d4f8b2
Kurt Kremitzki <kurt@kwk.systems>
2024-03-22 13:28:26 -0500

    Add python.lua note

--- dev/.config/nvim/after/ftplugin/python.lua
+++ dev/.config/nvim/after/ftplugin/python.lua
@@ -1,3 +1,5 @@
+-- TODO: Find an analog to this for snippets highlighted in visual mode.
+-- I want to be able to run chunks of code separate from file writes
 vim.keymap.set('n', '<F5>',
   '<cmd>5split +terminal\\ python3\\ -i\\ %<CR>',
   {noremap = true})
