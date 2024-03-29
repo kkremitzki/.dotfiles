8800967dd5e2969820826802f72936fe271c1c92
Kurt Kremitzki <kurt@kwk.systems>
2024-03-29 18:38:59 -0500

    Add ansible ft plugin

--- dev/.config/nvim/lua/plugins.lua
+++ dev/.config/nvim/lua/plugins.lua
@@ -324,6 +324,8 @@ return require('packer').startup(function(use)
     end
   }
 
+  use 'pearofducks/ansible-vim'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
